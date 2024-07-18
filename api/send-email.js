3e58b2f67346e0845e7d9d349d0789e752345d74
Alexis <alexis@Alexiss-MacBook-Pro.local>
2024-07-18 03:12:12 -0600

    ajsdasjd

--- api/send-email.js
+++ api/send-email.js
@@ -3,7 +3,11 @@ const { Pool } = require('pg');
 const nodemailer = require('nodemailer');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
     ssl: {
         rejectUnauthorized: false
     }
